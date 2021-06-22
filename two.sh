bcc531b6a62e3836f96ae8572fc1ef17aa08bd1e
Prem Surwase <prem.surwase17@gmail.com>
2021-06-22 14:17:53 +0530

    Updated the file two.sh

--- two.sh
+++ two.sh
@@ -1,3 +1,12 @@
+<<<<<<< HEAD
 1. This is awesome. 
 2. It's a beautiful day.
 3. Always keep smiling. 
+=======
+1. This is awesome.
+2. It's a beautiful day. 
+3. Always keep smiling.
+
+
+
+>>>>>>> Updated the file two.sh
